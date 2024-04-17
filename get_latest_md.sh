1a2c0ad93b0365e0d669b776e9a610831edc6cde
Filip Pröstl Andrén <filip.proestl-andren@ait.ac.at>
2024-04-17 12:09:00 +0200

    update workflow

--- get_latest_md.sh
+++ get_latest_md.sh
@@ -1,5 +1,5 @@
-set -o nounset
-set -o errexit
+# set -o nounset
+# set -o errexit
 
 artifact_download_url=$1
 
@@ -12,12 +12,42 @@ artifact_download_url=$1
 # echo "Release version: $release_version"
 # wget https://github.com/ERIGrid2/test-case-repository-word2md/releases/download/${release_version}/erigrid2-test-cases.zip
 
-wget ${artifact_download_url}
+echo "Getting: $artifact_download_url"
 
-unzip test-cases-markdown.zip -d content/en/docs/
+wget_received=8
+retry_count=0
+while [ $wget_received -eq 8 ]
+do
+    wget -q ${artifact_download_url}
+    wget_received=$?
+    echo "wget received: $wget_received"
+    if [ $wget_received -ne 0 ]
+    then
+        ((retry_count=$retry_count+1))
+        if [ $retry_count -gt 10 ];
+        then
+            break
+        else
+            sleep 5
+        fi
+    fi
+
+done
+if [ $wget_received -eq 0 ];
+then
+    unzip test-cases-markdown.zip -d content/en/docs/
+    rm test-cases-markdown.zip
+    exit 0
+fi
+exit 1
+
+
+# wget ${artifact_download_url}
+
+# unzip test-cases-markdown.zip -d content/en/docs/
 # for dir in $(find content/en/docs/erigrid2-test-cases/ -mindepth 1 -maxdepth 1 -type d);
 # do
 #   cp -a $dir content/en/docs/UseCases
 # done
-rm test-cases-markdown.zip
+# rm test-cases-markdown.zip
 # rm -rf content/en/docs/erigrid2-test-cases
